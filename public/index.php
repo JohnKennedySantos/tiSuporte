854239150c96f75487b48f524419f0c903b3baee
JohnKennedySantos <kennedyjohn2011@gmail.com>
2025-08-20 21:53:24 -0300

    Modelo MVCs

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php 
   
-  "Do it NOW!"
+  // INICIO DA SESSÃO
+    session_start();
+
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    require_once __DIR__ . '/../src/Config/Database.php';
+
+  use Src\Config\Database;
+
+  $db = new Database();
+  $conn = $db->getConnection();
+
+  if ($conn) {
+        echo "Conexão com o banco de dados estabelecida com sucesso!";
+    } else {
+        echo "Erro ao conectar com o banco de dados.";
+    }
+
 
 ?>
